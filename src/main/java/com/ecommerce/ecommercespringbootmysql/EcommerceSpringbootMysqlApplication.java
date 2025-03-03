34855bd7c23bd9778c103a5f4f8f8f390d8db1c3
Vương Trần <82163378+vuongtrann@users.noreply.github.com>
2025-03-03 23:26:41 +0700

    init project

---
+++ src/main/java/com/ecommerce/ecommercespringbootmysql/EcommerceSpringbootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommercespringbootmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbootMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSpringbootMysqlApplication.class, args);
+    }
+
+}
