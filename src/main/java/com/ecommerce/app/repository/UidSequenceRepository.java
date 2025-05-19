885691e69ed5858ba2e468029b987afaaafb91aa
Vương Trần <82163378+vuongtrann@users.noreply.github.com>
2025-05-19 23:54:25 +0700

    deleted uid_sequence

--- src/main/java/com/ecommerce/app/repository/UidSequenceRepository.java
+++ src/main/java/com/ecommerce/app/repository/UidSequenceRepository.java
@@ -1,6 +1,5 @@
 package com.ecommerce.app.repository;
 
-import com.ecommerce.app.model.entity.UidSequence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UidSequenceRepository extends JpaRepository<UidSequence, Long> {
