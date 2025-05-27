ab4452882fc93cb062ae4c9f7d016bd9e47e4b1b
Vương Trần <82163378+vuongtrann@users.noreply.github.com>
2025-05-28 00:50:31 +0700

    add model refreshtoken

---
+++ src/main/java/com/ecommerce/app/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.app.repository;
+
+import com.ecommerce.app.model.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository <RefreshToken, String>{
+    Optional<RefreshToken> findByToken(String token);
+    List<RefreshToken> findAllByUserId(Long userId);
+    void deleteByUserId(Long userId);
+}
